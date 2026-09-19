6f0c8dd452b2d4a7f5a0b4fe9a578e71dca19b6d
Prince Gabriel <princegabriel2022@gmail.com>
2026-09-19 07:45:59 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gabgate-v1-2-final-cache-v1';
+const CACHE_NAME = 'gabgate-v1-2-final-cache-v2';
 const ASSETS = ['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', e => {
